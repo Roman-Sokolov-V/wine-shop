3c4c18f4fdf102fb84d99083508baaa306673eeb
Roman Sokolov <gnonasis@gmail.com>
2025-06-14 20:58:46 +0300

    some chenges in Pet model

--- backend/pet/models.py
+++ backend/pet/models.py
@@ -1,5 +1,8 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.core.validators import MinValueValidator
 
+#User = get_user_model()
 
 SEX_CHOICES = [
         ('M', 'Male'),
@@ -9,20 +12,25 @@ SEX_CHOICES = [
 
 class Pet(models.Model):
     name = models.CharField(max_length=100)
-    type = models.CharField(max_length=100)
-    age = models.IntegerField(null=True, blank=True)
+    pet_type = models.CharField(max_length=100)
+    age = models.PositiveIntegerField(null=True, blank=True)
     breed = models.CharField(max_length=100, null=True, blank=True)
     sex = models.CharField(max_length=10, choices=SEX_CHOICES)
     coloration = models.CharField(max_length=50, null=True, blank=True)
-    weight = models.IntegerField(null=True, blank=True)
+    weight = models.FloatField(null=True, blank=True, validators=[MinValueValidator(0.01)])
     is_sterilized = models.BooleanField(null=True, blank=True, default=None)
     description = models.TextField(null=True, blank=True)
+    date_created = models.DateField(auto_now_add=True)
+    #owner = models.ForeignKey(User, on_delete=models.CASCADE, unique=True, default=None)
 
     class Meta:
         verbose_name_plural = "pets"
 
+
     def __str__(self):
         return f"id {self.id}, a {self.type} named {self.name.capitalize()}"
+
+
 
 
 
