bcf9e7fcabdad9541f155479c1ca66960b6544fc
Roman Sokolov <gnonasis@gmail.com>
2025-06-14 19:04:50 +0300

    add model Pet

--- backend/pet/models.py
+++ backend/pet/models.py
@@ -1,7 +1,23 @@
 from django.db import models
 
+
+SEX_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+    ]
+
+
 class Pet(models.Model):
     name = models.CharField(max_length=100)
     type = models.CharField(max_length=100)
+    age = models.IntegerField(null=True, blank=True)
+    breed = models.CharField(max_length=100, null=True, blank=True)
+    sex = models.CharField(max_length=10, choices=SEX_CHOICES)
+    coloration = models.CharField(max_length=50, null=True, blank=True)
+    weight = models.IntegerField(null=True, blank=True)
+    is_sterilized = models.BooleanField(null=True, blank=True, default=None)
+    description = models.TextField(null=True, blank=True)
+
+
 
 
