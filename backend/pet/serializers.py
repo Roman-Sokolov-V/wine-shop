c4cce663b300a106c7333e865208bc08b2b49302
Roman Sokolov <gnonasis@gmail.com>
2025-06-14 20:57:15 +0300

    add validation to PetSerializer

--- backend/pet/serializers.py
+++ backend/pet/serializers.py
@@ -1,4 +1,7 @@
+from datetime import date
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
 
 from pet.models import Pet
 
@@ -9,7 +12,7 @@ class PetSerializer(serializers.ModelSerializer):
         fields = (
             "id",
             "name",
-            "type",
+            "pet_type",
             "age",
             "breed",
             "sex",
@@ -19,3 +22,36 @@ class PetSerializer(serializers.ModelSerializer):
             "description",
             #"owner"
         )
+
+    def validate(self, data):
+        if self.instance is None:
+            name = data.get("name")
+            pet_type = data.get("pet_type")
+            age = data.get("age")
+            breed = data.get("breed")
+            sex = data.get("sex")
+            coloration = data.get("coloration")
+            weight = data.get("weight")
+            is_sterilized = data.get("is_sterilized")
+            description = data.get("description")
+            date_added = date.today()
+
+            if Pet.objects.filter(
+                name=name,
+                pet_type=pet_type,
+                age=age,
+                breed=breed,
+                sex=sex,
+                coloration=coloration,
+                weight=weight,
+                is_sterilized=is_sterilized,
+                description=description,
+                date_created=date_added,
+
+            ).exists():
+                raise ValidationError(
+                    "Pet with the same parameters already added to db today. "
+                    "Make sure you not try to add the same pet by mistake. "
+                    "If not just change any field"
+                )
+                return data
