b83a15b0aef8b7a7c564c21831b4b0b10e4e6dba
Roman Sokolov <gnonasis@gmail.com>
2025-06-15 17:13:00 +0300

    add Image model

--- backend/pet/models.py
+++ backend/pet/models.py
@@ -1,13 +1,15 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.core.validators import MinValueValidator
-
-#User = get_user_model()
+from storages.backends.s3boto3 import S3Boto3Storage
+# User = get_user_model()
 
 SEX_CHOICES = [
-        ('M', 'Male'),
-        ('F', 'Female'),
-    ]
+    ("M", "Male"),
+    ("F", "Female"),
+]
+
+
 
 
 class Pet(models.Model):
@@ -17,21 +19,21 @@ class Pet(models.Model):
     breed = models.CharField(max_length=100, null=True, blank=True)
     sex = models.CharField(max_length=10, choices=SEX_CHOICES)
     coloration = models.CharField(max_length=50, null=True, blank=True)
-    weight = models.FloatField(null=True, blank=True, validators=[MinValueValidator(0.01)])
+    weight = models.FloatField(
+        null=True, blank=True, validators=[MinValueValidator(0.01)]
+    )
     is_sterilized = models.BooleanField(null=True, blank=True, default=None)
     description = models.TextField(null=True, blank=True)
     date_created = models.DateField(auto_now_add=True)
-    #owner = models.ForeignKey(User, on_delete=models.CASCADE, unique=True, default=None)
+    # owner = models.ForeignKey(User, on_delete=models.CASCADE, unique=True, default=None)
 
     class Meta:
         verbose_name_plural = "pets"
 
-
     def __str__(self):
-        return f"id {self.id}, a {self.type} named {self.name.capitalize()}"
+        return f"id {self.id}, a {self.pet_type} named {self.name.capitalize()}"
 
 
-
-
-
-
+class Image(models.Model):
+    file = models.ImageField(upload_to="images/")
+    pet = models.ForeignKey(Pet, on_delete=models.CASCADE, related_name="images")
