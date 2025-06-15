63fe70e04925e1522bb93cbaaa8b6dd44f0b3bee
Roman Sokolov <gnonasis@gmail.com>
2025-06-15 17:16:06 +0300

    add route for upload image

--- backend/pet/urls.py
+++ backend/pet/urls.py
@@ -1,18 +1,15 @@
 from django.urls import path
-from pet.views import PetViewSet
+from pet.views import PetViewSet, UploadImageView
 
 app_name = "pet"
 
 pets_list_create = PetViewSet.as_view(actions={"get": "list", "post": "create"})
 pet_detail = PetViewSet.as_view(
-    actions={
-        "get": "retrieve",
-        "patch": "partial_update",
-        "delete": "destroy"
-    }
+    actions={"get": "retrieve", "patch": "partial_update", "delete": "destroy"}
 )
 
 urlpatterns = [
-    path("", pets_list_create, name="pets_list_create"),
-    path("<int:pk>/", pet_detail, name="pet_detail"),
+    path("", pets_list_create, name="pets-list"),
+    path("<int:pk>/", pet_detail, name="pet-detail"),
+    path("upload/", UploadImageView.as_view(), name="upload-image"),
 ]
