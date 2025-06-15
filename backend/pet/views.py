a11b7521d8c784f86ecd09a21a8586cda75359aa
Roman Sokolov <gnonasis@gmail.com>
2025-06-15 17:15:31 +0300

    add UploadImageView

--- backend/pet/views.py
+++ backend/pet/views.py
@@ -1,9 +1,15 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 
-from pet.models import Pet
-from pet.serializers import PetSerializer
+
+from pet.models import Pet, Image
+from pet.serializers import PetSerializer, UploadImageSerializer
 
 
 class PetViewSet(viewsets.ModelViewSet):
     queryset = Pet.objects.all()
     serializer_class = PetSerializer
+
+
+class UploadImageView(generics.ListCreateAPIView):
+    queryset = Image.objects.all()
+    serializer_class = UploadImageSerializer
