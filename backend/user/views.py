fe7ec53004cad7e949267105ad43ffd80ad6c87d
Roman Sokolov <gnonasis@gmail.com>
2025-06-16 15:45:59 +0300

    add UserViewSet  LoginView

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
-# Create your views here.
+from backend.user.serializers import UserSerializer, LoginSerializer
+
+User = get_user_model()
+
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class LoginView(ObtainAuthToken):
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    serializer_class = LoginSerializer
