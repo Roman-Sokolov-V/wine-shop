2b22a8d5f783cd9920819fcb6468e1f6eff8d3a6
Roman Sokolov <gnonasis@gmail.com>
2025-06-21 15:54:14 +0300

    add TemporaryTokenView, UpdatePasswordView

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,18 +1,37 @@
 from django.contrib.auth import get_user_model
+from django.core.serializers import get_serializer
 from rest_framework import viewsets, permissions, generics, status
 from rest_framework.response import Response
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 from rest_framework.authtoken.models import Token
+from rest_framework.views import APIView
+from drf_spectacular.utils import extend_schema, extend_schema_view
 
-
-from user.serializers import UserSerializer, LoginSerializer
+from notification.notification import send_email_restore_password_token
+from user.models import TempToken
+from user.serializers import (
+    UserSerializer,
+    LoginSerializer,
+    TempTokenSerializer,
+    UpdatePasswordSerializer
+)
 from user.permissions import IsOwner
 
 User = get_user_model()
 
-
+@extend_schema_view(
+    list=extend_schema(description="Get the list of all users."),
+    retrieve=extend_schema(description="Retrieve a specific user by ID."),
+    create=extend_schema(description="Create a new user."),
+    update=extend_schema(description="Fully update a user."),
+    partial_update=extend_schema(description="Partially update a user."),
+    destroy=extend_schema(description="Delete a user."),
+)
 class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
     serializer_class = UserSerializer
     queryset = User.objects.all()
 
@@ -30,8 +49,7 @@ class UserViewSet(viewsets.ModelViewSet):
 
 class LoginView(ObtainAuthToken):
     """
-    Login view that return response with TokenAuthentication for
-    authentication a user.
+    Login view that return response with TokenAuthentication and user data.
     """
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
     serializer_class = LoginSerializer
@@ -53,7 +71,6 @@ class LoginView(ObtainAuthToken):
                 'is_superuser': user.is_superuser,
                 'is_active': user.is_active,
                 "date_joined": user.date_joined,
-
             }
         )
 
@@ -86,3 +103,47 @@ class MeView(generics.GenericAPIView):
         serializer = self.get_serializer(user)
         return Response(serializer.data)
 
+@extend_schema(
+    request=TempTokenSerializer,
+    responses={201: None}
+)
+class TemporaryTokenView(APIView):
+    """
+    Endpoint to create a temporary token to update forgotten password
+    """
+    permission_classes = [permissions.AllowAny]
+    serializer_class = TempTokenSerializer
+
+    def post(self, request, *args, **kwargs):
+        print("=== TEMPORARY TOKEN VIEW HIT ===")
+        #print("user:", request.user)
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        validated_data = serializer.validated_data
+        user = validated_data['user']
+        TempToken.objects.filter(user=user).delete()
+        token = TempToken.objects.create(user=user)
+        send_email_restore_password_token(token=token.key, user=user)
+        return Response(status=status.HTTP_201_CREATED)
+
+
+
+class UpdatePasswordView(APIView):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = UpdatePasswordSerializer
+    """
+    Endpoint to update forgotten password
+    """
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            validated_data = serializer.validated_data
+            temp_token = validated_data["temp_token"]
+            user = temp_token.user
+            serializer.update(instance=user, validated_data=validated_data)
+            temp_token.delete()
+            return Response({"detail": "Password has been updated"},
+                            status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
