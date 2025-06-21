e1120446f4b99c0f606e95dc520b30bafc65d35c
Roman Sokolov <gnonasis@gmail.com>
2025-06-21 15:59:10 +0300

    add restore_token update_password endpoints

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from user.views import UserViewSet, LoginView, LogoutView, MeView
-
+from user.views import (
+    UserViewSet,
+    LoginView,
+    LogoutView,
+    MeView,
+    TemporaryTokenView,
+    UpdatePasswordView
+)
 app_name = "user"
 
 user_create = UserViewSet.as_view(actions={"post": "create"})
@@ -17,7 +23,10 @@ urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
     path("register/", user_create, name="register"),
     path("logout/", LogoutView.as_view(), name="logout"),
+    path("users/", users_list, name="users"),
     path("me/", MeView.as_view(), name="me"),
-    path("<pk>/", user_detail, name="user-detail"),
+    path("<int:pk>/", user_detail, name="user-detail"),
+    path("restore_token/", TemporaryTokenView.as_view(), name="restore-token"),
+    path("update_password/", UpdatePasswordView.as_view(), name="update-password"),
     path("", users_list, name="users-list"),
 ]
