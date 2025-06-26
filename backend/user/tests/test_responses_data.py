17a5806e86e9ddae831222b5d338f82f311f5bab
Roman Sokolov <gnonasis@gmail.com>
2025-06-26 15:34:32 +0300

    update ci

--- backend/user/tests/test_responses_data.py
+++ backend/user/tests/test_responses_data.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from rest_framework import status
-from rest_framework.test import APIClient, force_authenticate
+from rest_framework.test import APIClient
 from rest_framework.reverse import reverse
 from rest_framework.authtoken.models import Token
 from django.contrib.auth import get_user_model
