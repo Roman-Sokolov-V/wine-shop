1b980b0a61e6ea5681ac213be421cede17431d92
Roman Sokolov <gnonasis@gmail.com>
2025-06-29 11:22:28 +0300

    add task create_subscriptions for automatic subscribe registered users

--- backend/user/tasks.py
+++ backend/user/tasks.py
@@ -2,6 +2,8 @@ import dramatiq
 import logging
 from django.contrib.auth import get_user_model
 
+import user
+from notification.models import Mailing, Subscription
 from notification.notification import send_email_restore_password_token
 from user.models import TempToken
 
@@ -11,8 +13,22 @@ User = get_user_model()
 
 @dramatiq.actor
 def send_restore_token(token: str, user_email: str):
+    """send email with token for update forgotten password"""
     try:
         send_email_restore_password_token(token=token, user_email=user_email)
     except Exception as e:
         logger.error(f"Failed to send email to {user_email}: {str(e)}")
         raise
+
+
+@dramatiq.actor
+def create_subscriptions(email: str, user_id: int) -> None:
+    """create subscriptions with all mailing"""
+    mailings_ids = Mailing.objects.all().values_list("id", flat=True)
+    if mailings_ids:
+        Subscription.objects.bulk_create(
+            (
+                Subscription(email=email, mailing_id=mailing_id, user_id=user_id)
+                for mailing_id in mailings_ids
+            )
+        )
