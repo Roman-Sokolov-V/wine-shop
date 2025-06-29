319c47ed5306fc57ca3ff17b8266625db8eaa1e7
Roman Sokolov <gnonasis@gmail.com>
2025-06-29 11:18:57 +0300

    add SubscriptionViewSet and MailingViewSet

--- backend/notification/views.py
+++ backend/notification/views.py
@@ -1,31 +1,42 @@
-# from rest_framework import viewsets, mixins
-# from rest_framework.permissions import IsAdminUser, AllowAny
-# from rest_framework.viewsets import GenericViewSet
-#
-# from notification.models import Subscription, Mailing
-# from notification.permissions import IsOwnerOrAdmin
-# from notification.serializers import SubscriptionSerializer, MailingSerializer
-#
-#
-# class SubscriptionViewSet(
-#     mixins.CreateModelMixin,
-#     mixins.DestroyModelMixin,
-#     mixins.ListModelMixin,
-#     GenericViewSet,
-# ):
-#     queryset = Subscription.objects.all()
-#     serializer_class = SubscriptionSerializer
-#
-#     def get_permissions(self):
-#         if self.action == "destroy":
-#             return (IsOwnerOrAdmin(),)
-#         elif self.action == "list":
-#             return (IsAdminUser(),)
-#         else:
-#             return (AllowAny(),)
-#
-#
-# class MailingViewSet(viewsets.ModelViewSet):
-#     queryset = Mailing.objects.all()
-#     serializer_class = MailingSerializer
-#     permission_classes = (IsAdminUser,)
+from rest_framework import viewsets, mixins
+from rest_framework.permissions import IsAdminUser, AllowAny, SAFE_METHODS
+from rest_framework.viewsets import GenericViewSet
+
+from notification.models import Subscription, Mailing
+from notification.permissions import IsOwnerOrAdmin
+from notification.serializers import SubscriptionSerializer, MailingSerializer
+
+
+class SubscriptionViewSet(
+    mixins.CreateModelMixin,
+    mixins.DestroyModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = Subscription.objects.all()
+    serializer_class = SubscriptionSerializer
+
+    def get_permissions(self):
+        if self.action == "destroy":
+            return (IsOwnerOrAdmin(),)
+        elif self.action == "list":
+            return (IsAdminUser(),)
+        else:
+            return (AllowAny(),)
+
+
+class MailingViewSet(
+    mixins.CreateModelMixin,
+    mixins.DestroyModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = Mailing.objects.all()
+    serializer_class = MailingSerializer
+
+    def get_permissions(self):
+        if self.action in ("list", "retrieve"):
+            return (AllowAny(),)
+        else:
+            return (IsAdminUser(),)
