86bc37b389ed9f7fb8deca52a3b4592905e552f6
Roman Sokolov <gnonasis@gmail.com>
2025-06-30 17:47:34 +0300

    add send_mailing_task - celery-beat task

---
+++ backend/notification/tasks.py
@@ -0,0 +1,28 @@
+from celery import shared_task
+
+from notification.models import Subscription
+from notification.notification import send_mailing
+
+from django.conf import settings
+
+UNSUBSCRIBE_URL = settings.FRONT_HOST + settings.UNSUBSCRIBE_URL
+
+
+@shared_task
+def send_mailing_task(mailing_id: int):
+    subscriptions = Subscription.objects.filter(mailing_id=mailing_id).select_related(
+        "user", "mailing"
+    )
+    for subscription in subscriptions:
+        email = subscription.email or (
+            subscription.user.email if subscription.user else None
+        )
+        if not email:
+            continue  # або логувати warning
+        send_mailing(
+            user_email=email,
+            topic=subscription.mailing.title,
+            content=subscription.mailing.content,
+            token=subscription.token,
+            unscribe_url=UNSUBSCRIBE_URL,  # TODO: реальний URL
+        )
