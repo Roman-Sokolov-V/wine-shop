7e4d31f9f6ca4e4ef0b52879e9449bb621563239
Roman Sokolov <gnonasis@gmail.com>
2025-07-04 12:56:32 +0300

    update AdoptionFormSerializer

--- backend/adoption/serializers.py
+++ backend/adoption/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers, status
 from rest_framework.exceptions import ValidationError
 
 from adoption.models import Appointment, AdoptionForm
+from pet.models import Pet
 
 
 class AppointmentSerializer(serializers.ModelSerializer):
@@ -27,12 +28,33 @@ class AppointmentSerializer(serializers.ModelSerializer):
 
 
 class AdoptionFormSerializer(serializers.ModelSerializer):
+    pet = serializers.PrimaryKeyRelatedField(queryset=Pet.objects.all())
+    user = serializers.PrimaryKeyRelatedField(read_only=True)
+
     class Meta:
         model = AdoptionForm
-        fields = "__all__"
+        fields = (
+            "user",
+            "pet",
+            "application_date",
+            "first_name",
+            "last_name",
+            "address",
+            "phone",
+            "email",
+            "occupation",
+            "employer_name",
+            "employer_phone",
+            "living_situation",
+            "household_setting",
+            "household_members",
+            "fenced_yard",
+            "hours_alone",
+            "status",
+        )
         extra_kwargs = {
             "application_date": {"read_only": True},
-            "user_id": {"read_only": True},
+            "user": {"read_only": True},
         }
 
     def get_extra_kwargs(self, *args, **kwargs):
@@ -42,15 +64,12 @@ class AdoptionFormSerializer(serializers.ModelSerializer):
         - Makes the 'status' field read-only during creation (when instance is None).
         - For authenticated users, sets 'email' as read-only to prevent client modification.
         - For unauthenticated users, sets 'email' as required to ensure it is provided.
-
-        This approach enforces backend control over sensitive fields while maintaining
-        validation requirements based on authentication state.
         """
         extra_kwargs = super().get_extra_kwargs()
         if self.instance is None:
             extra_kwargs["status"] = {"read_only": True}
-        request = self.context.get("request")
 
+        request = self.context.get("request")
         if request and request.user.is_authenticated:
             extra_kwargs["email"] = {"read_only": True}
         else:
@@ -59,24 +78,16 @@ class AdoptionFormSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         """
-        Automatically assign 'user_id' and 'email' from the authenticated user,
+        Automatically assign 'user' and 'email' from the authenticated user,
         if available, when creating a new AdoptionForm instance.
 
-        If the user is not authenticated, 'user_id' will remain unset (and can be null).
-        This avoids relying on client input for these sensitive fields.
+        If the user is not authenticated, 'user' will remain unset (and can be null).
         """
         request = self.context.get("request")
         if request and request.user.is_authenticated:
-            validated_data["user_id"] = self.context["request"].user.id
-            validated_data["email"] = self.context["request"].user.email
+            validated_data["user"] = request.user
+            validated_data["email"] = request.user.email
         return super().create(validated_data)
-
-    def validate_pet_id(self, pet_id):
-        if not AdoptionForm.objects.filter(id=pet_id).exists():
-            raise ValidationError(
-                detail=f"Pet with id {pet_id} does not exist.",
-                code=status.HTTP_404_NOT_FOUND,
-            )
 
 
 class AdoptionUpdateStatusSerializer(serializers.ModelSerializer):
